b6cb12e3e0b6f7e6eed0b41a17027df080678d62
farzanahossain906 <farzanahossain906@gmail.com>
2023-03-28 18:55:57 +0600

    Homework complete

--- src/InterchangedValue.java
+++ src/InterchangedValue.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class InterchangedValue {
     public static void main(String[] args) {
+        //declare variables
         int a, b,c ;
         Scanner scanner=new Scanner(System.in);
         //acknowledge the user what to input
